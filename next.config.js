e48e0ed8370d16f091aa5dca950deccc5ca5a1be
Som3sh <someshmohanty08@gmail.com>
2024-01-24 20:52:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      // add more routes here
-    };
-  },
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
